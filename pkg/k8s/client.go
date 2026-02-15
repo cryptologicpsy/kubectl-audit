46b3b02fa7fcd0790f6d5b00ff995804fc63a789
George <101523970+cryptologicpsy@users.noreply.github.com>
2026-02-15 17:47:59 +0200

    Refactor NewClient function comment
    Remove Greek comment and update NewClient function.

--- pkg/k8s/client.go
+++ pkg/k8s/client.go
@@ -5,7 +5,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// NewClient δημιουργεί Kubernetes client από kubeconfig
+// NewClient 
 func NewClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
